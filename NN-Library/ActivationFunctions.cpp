45d74408a4cb8fb403496f99bde02b8bcb7fbbe8
Nasser Ksous <55693958+NasserKsous@users.noreply.github.com>
2022-03-31 10:26:10 +0100

    removed parametric relu

--- NN-Library/ActivationFunctions.cpp
+++ NN-Library/ActivationFunctions.cpp
@@ -1,54 +1,5 @@
 #include "ActivationFunctions.h"
 
-//float ActivationFunction::Activate(float x, ACTIVATION type)
-//{
-//	switch (type)
-//	{
-//	case ACTIVATION::LINEAR:
-//		{
-//			return LinearActivation(x);
-//			break;
-//		}
-//		case ACTIVATION::BINARY_STEP:
-//		{
-//			return BinaryStepActivation(x);
-//			break;
-//		}
-//		case ACTIVATION::SIGMOID:
-//		{
-//			return SigmoidActivation(x);
-//			break;
-//		}
-//		case ACTIVATION::TANH:
-//		{
-//			if (x < -1.0f)
-//				return -1.0f;
-//			if (x > 1.0f)
-//				return 1.0f;
-//			return TanhActivation(x);
-//			break;
-//		}
-//		case ACTIVATION::RELU:
-//		{
-//			return ReLUActivation(x);
-//			break;
-//		}
-//		case ACTIVATION::LEAKY_RELU:
-//		{
-//			return LeakyReLUActivation(x);
-//			break;
-//		}
-//		case ACTIVATION::PARAMETRIC_RELU:
-//		{
-//			return ParametricReLUActivation(x, alpha);
-//			break;
-//		}
-//
-//		
-//	}
-//	return 0.0f;
-//}
-
 float Activate(float x, ACTIVATION type)
 {
 	switch (type)
--- NN-Library/ActivationFunctions.h
+++ NN-Library/ActivationFunctions.h
@@ -9,7 +9,6 @@ static inline float SigmoidActivation(float x) { return 1.0f / (1.0f + expf(-x))
 static inline float TanhActivation(float x) { return (expf(x) - expf(-x))/(expf(x) + expf(-x)); }
 static inline float ReLUActivation(float x) { return (x >= 0) ? x : 0.0f; }
 static inline float LeakyReLUActivation(float x) { return (x >= 0) ? x : 0.1f*x; }
-static inline float ParametricReLUActivation(float x, float alpha) { return (x >= 0) ? x : alpha * x; }
 
 float Activate(float x, ACTIVATION type);
 
