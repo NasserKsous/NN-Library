7b2793e5f9556c4e19dd677f42bf3833e8d5e51e
Nasser Ksous <55693958+NasserKsous@users.noreply.github.com>
2022-04-06 18:29:28 +0100

    added derivations of activation functions

--- NN-Library/ActivationFunctions.cpp
+++ NN-Library/ActivationFunctions.cpp
@@ -41,3 +41,45 @@ float Activate(float x, ACTIVATION type)
 	}
 	return 0.0f;
 }
+
+float Deactivate(float x, ACTIVATION type)
+{
+	switch (type)
+	{
+	case ACTIVATION::LINEAR:
+	{
+		return LinearDeactivation(x);
+		break;
+	}
+	case ACTIVATION::BINARY_STEP:
+	{
+		return BinaryStepDeactivation(x);
+		break;
+	}
+	case ACTIVATION::SIGMOID:
+	{
+		return SigmoidDeactivation(x);
+		break;
+	}
+	case ACTIVATION::TANH:
+	{
+		if (x < -1.0f)
+			return -1.0f;
+		if (x > 1.0f)
+			return 1.0f;
+		return TanhDeactivation(x);
+		break;
+	}
+	case ACTIVATION::RELU:
+	{
+		return ReLUDeactivation(x);
+		break;
+	}
+	case ACTIVATION::LEAKY_RELU:
+	{
+		return LeakyReLUDeactivation(x);
+		break;
+	}
+	}
+	return 0.0f;
+}
